24a01c243be394c6ed6e91b02d2ac12eb072341b
Bernardo Brik <bernardobrik@gmail.com>
2013-08-13 13:32:26 -0300

    corrects app name on import and setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from setuptools import setup, find_packages
 
-import accounts
+import pjax
 
 
 setup(
     name='django-pjax-middleware',
-    version=accounts.__version__,
+    version=pjax.__version__,
     author=u'Bernardo Brik',
     author_email='bernardobrik@gmail.com',
     url='https://github.com/bbrik/django-pjax-middleware',
