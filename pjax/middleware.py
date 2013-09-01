783e13a2d2b5b2a33d8439b22913a9f86a63707c
Bernardo Brik <bernardobrik@gmail.com>
2013-09-01 11:22:51 -0300

    fix bug on final url in redirects

--- pjax/middleware.py
+++ pjax/middleware.py
@@ -13,6 +13,9 @@ class PjaxMiddleware(object):
         if request.META.get('HTTP_X_PJAX', False):
             logger.debug('pjax response')
             response.context_data['base'] = PJAX_BASE_TEMPLATE
+             # fix redirect problem:
+            response['X-PJAX-URL'] = '%s?%s' % \
+                                     (request.path, request.META['QUERY_STRING'])
         else:
             logger.debug('full response')
             response.context_data['base'] = BASE_TEMPLATE
