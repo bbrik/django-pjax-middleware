8cd5e4e01a994082444dc7731633b0342a14f42f
Bernardo Brik <bernardobrik@gmail.com>
2015-03-13 14:39:39 -0300

    Enables setting content version on response

--- pjax/base.py
+++ pjax/base.py
@@ -5,4 +5,4 @@ from django.conf import settings
 
 PJAX_BASE_TEMPLATE = getattr(settings, 'PJAX_BASE_TEMPLATE', 'pjax_base.html')
 BASE_TEMPLATE = getattr(settings, 'BASE_TEMPLATE', 'base.html')
-
+PJAX_VERSION = getattr(settings, 'PJAX_VERSION', None)
--- pjax/middleware.py
+++ pjax/middleware.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from .base import PJAX_BASE_TEMPLATE, BASE_TEMPLATE
+from .base import PJAX_BASE_TEMPLATE, BASE_TEMPLATE, PJAX_VERSION
 
 
 logger = logging.getLogger(__name__)
@@ -13,9 +13,12 @@ class PjaxMiddleware(object):
         if request.META.get('HTTP_X_PJAX', False):
             logger.debug('pjax response')
             response.context_data['base'] = PJAX_BASE_TEMPLATE
-             # fix redirect problem:
-            response['X-PJAX-URL'] = '%s?%s' % \
-                                     (request.path, request.META['QUERY_STRING'])
+            # fix redirect problem
+            url = '%s?%s' % (request.path, request.META['QUERY_STRING'])
+            response['X-PJAX-URL'] = url
+            # set version
+            if PJAX_VERSION:
+                response['X-PJAX-Version'] = PJAX_VERSION
         else:
             logger.debug('full response')
             response.context_data['base'] = BASE_TEMPLATE
